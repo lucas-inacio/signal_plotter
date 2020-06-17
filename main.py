730e6437593dbcde237a2b1521a39a760caca25c
Lucas Inácio Viegas <lucas.viegas@edu.pucrs.br>
2020-06-17 14:28:48 -0300

    Construção dos menus movida para o método buildMenu

--- main.py
+++ main.py
@@ -16,30 +16,8 @@ class MainFrame(tk.Frame):
         self.master = master
         self.grid()
         
-        # Menus
-        self.menubar = tk.Menu(self)
-        self.master['menu'] = self.menubar
-        menu_file = tk.Menu(self.menubar)
-        # Menu Arquivo
-        self.menubar.add_cascade(menu=menu_file, label='Arquivo')
-        menu_file.add_command(label='Abrir', command=self.openFile)
-        menu_file.add_command(label='Iniciar captura',
-                              command=self.startComDialog)
-        menu_file.add_command(label='Parar captura',
-                              command=self.stopCapture)
-        # Menu Bateria
-        self.currentBattery = 1
-        self.batterySelector = tk.IntVar()
-        self.batterySelector.set(1)
-        self.callbackName = self.batterySelector.trace_add(
-            'write', self.onBatteryChange)
-        menu_view = tk.Menu(self.menubar)
-        self.menubar.add_cascade(menu=menu_view, label='Bateria')
-        for i in range(1, 13):
-            menu_view.add_radiobutton(label=str(i),
-                                      variable=self.batterySelector)
-        # Ao fechar a janela
         self.master.protocol("WM_DELETE_WINDOW", self.closeWindow)
+        self.buildMenu()
 
         # Aquisição
         self.serialPort = SerialPort.SerialPort()
@@ -145,6 +123,30 @@ class MainFrame(tk.Frame):
             self.curve.setData(x, y)
         else:
             tk.messagebox.showerror(message='Arquivo incompatível')
+
+    def buildMenu(self):
+        # Menus
+        self.menubar = tk.Menu(self)
+        self.master['menu'] = self.menubar
+        menu_file = tk.Menu(self.menubar)
+        # Menu Arquivo
+        self.menubar.add_cascade(menu=menu_file, label='Arquivo')
+        menu_file.add_command(label='Abrir', command=self.openFile)
+        menu_file.add_command(label='Iniciar captura',
+                              command=self.startComDialog)
+        menu_file.add_command(label='Parar captura',
+                              command=self.stopCapture)
+        # Menu Bateria
+        self.currentBattery = 1
+        self.batterySelector = tk.IntVar()
+        self.batterySelector.set(1)
+        self.callbackName = self.batterySelector.trace_add(
+            'write', self.onBatteryChange)
+        menu_view = tk.Menu(self.menubar)
+        self.menubar.add_cascade(menu=menu_view, label='Bateria')
+        for i in range(1, 13):
+            menu_view.add_radiobutton(label=str(i),
+                                      variable=self.batterySelector)
 
 def main():
     root = tk.Tk()
